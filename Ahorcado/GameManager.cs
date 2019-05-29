06419e41f7b5e3818f8d3219516623aba27c8000
Lazaro Ansaldi <lansaldi@flexiti.com>
2019-05-28 22:07:36 -0300

    Initial Commit

---
+++ Ahorcado/GameManager.cs
@@ -0,0 +1,27 @@
+﻿namespace Ahorcado
+{
+    public class GameManager
+    {
+        public string SecretWord { get; }
+        public int SecretCant { get { return SecretWord.Length; } }
+        public int CantidadErrores { get; set; }
+
+        public GameManager(string secretWord)
+        {
+            SecretWord = secretWord;
+            CantidadErrores = 0;
+        }
+
+        public bool ContieneLetra(string secretLetra)
+        {
+            var result = SecretWord.Contains(secretLetra);
+
+            if (result == false)
+            {
+                CantidadErrores++;
+            }
+
+            return result;
+        }
+    }
+}
