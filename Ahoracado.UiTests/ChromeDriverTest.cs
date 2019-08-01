3271628fcb4bd794a8f0cf7f34348f531c7a260b
Lazaro Ansaldi <lansaldi@flexiti.com>
2019-08-01 12:05:27 -0300

    Add Selenium tests for Chrome

---
+++ Ahoracado.UiTests/ChromeDriverTest.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Ahoracado.UiTests
+{
+    [TestClass]
+    public class ChromeDriverTest
+    {
+        private ChromeDriver _driver;
+
+        #region Page Controls
+        private IWebElement TxtNombreJugador => _driver.FindElementByName("PlayerName");
+        private IWebElement TxtSecretWord => _driver.FindElementByName("SecretWord");
+        private IWebElement BtnStart => _driver.FindElementByName("Start");
+        private IWebElement TxtTryLetter => _driver.FindElementByName("LetterToTry");
+        private IWebElement BtnTryLetter => _driver.FindElementByName("BtnStartGame");
+        private IWebElement TxtLetrasAcertadas => _driver.FindElementByName("LetrasAcertadas");
+        #endregion
+
+        [TestInitialize]
+        public void EdgeDriverInitialize()
+        {
+            _driver = new ChromeDriver(@"C:\Projects\UTN\utn-ahorcado\Ahoracado.UiTests\bin\Debug");
+        }
+
+        [TestMethod]
+        public void VerifyPageTitle()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:44310/Home/Index");
+            // Start Page
+            TxtNombreJugador.SendKeys("Lazaro");
+            TxtSecretWord.SendKeys("a");
+            BtnStart.Click();
+
+            TxtTryLetter.SendKeys("a");
+            BtnTryLetter.Click();
+
+            Assert.AreEqual("1", TxtLetrasAcertadas.Text);
+        }
+
+        [TestCleanup]
+        public void EdgeDriverCleanup()
+        {
+            _driver.Quit();
+        }
+    }
+}
